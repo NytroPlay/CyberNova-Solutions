ddad1f31cc7dc99046e41907ab51d9c83d80ad3a
jhonider aguilar <jhonider2013@hotmail.com>
2025-09-22 16:17:53 -0500

    refactor: mejorar comentario sobre la configuración de la base en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 👇 Ajusta la base con el nombre de tu repo
+// 👇 Ajusta la base con el nombre EXACTO de tu repo en GitHub
 export default defineConfig({
   plugins: [react()],
   base: '/CyberNova-Solutions/'
