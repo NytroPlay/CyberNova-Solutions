11a0dc41dba88a79a9c7fd94f14040b94a712567
jhonider aguilar <jhonider2013@hotmail.com>
2025-09-22 13:21:20 -0500

    feat: configuracion para gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 // src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // <- Para GitHub Pages sin 404
+import { BrowserRouter } from "react-router-dom"; 
 import App from "./App";
 import "./styles/styles.css";
 
-
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/CyberNova-Solutions/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/CyberNova-Solutions/", // 👈 nombre EXACTO de tu repo
+  base: '/CyberNova-Solutions/', // Debe coincidir con el nombre de tu repositorio
 });
