14f9f4560ab2cbf142d3a9e7774221f3f3efb444
jhonider aguilar <jhonider2013@hotmail.com>
2025-09-22 15:24:17 -0500

    fix: corregir formato y comentarios en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/CyberNova-Solutions/", // 👈 importante para GitHub Pages
-});
+  base: '/CyberNova-Solutions/', // 👈 nombre exacto del repo en GitHub
+  build: {
+    outDir: 'docs' // 👈 exporta directamente a /docs
+  }
+})
