18f05c5f991857b1e58aab6b0b102559e1a9efe2
jhonider aguilar <jhonider2013@hotmail.com>
2025-09-22 13:06:16 -0500

    configuración inicial de Vite para el proyecto CyberNova-Solutions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/CyberNova-Solutions/", // 👈 importante: debe coincidir con el nombre del repo
+  base: "/CyberNova-Solutions/", // 👈 nombre EXACTO de tu repo
 });
