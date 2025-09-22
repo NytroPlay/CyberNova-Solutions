9d5e995f594bbf92278046bfafb1804c29b87c3e
jhonider aguilar <jhonider2013@hotmail.com>
2025-09-22 13:25:12 -0500

    fix: corregir el nombre de la base en la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CyberNova-Solutions/', // Debe coincidir con el nombre de tu repositorio
+  // El "base" debe ser el nombre EXACTO de tu repositorio
+  base: "/CyberNova Solutions/",
 });
