131e45928b8ca8b8664d09a594ceccbb63d66478
jhonider aguilar <jhonider2013@hotmail.com>
2025-09-22 21:48:36 -0500

    vamos

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-// src/main.jsx - VERSIÓN FINAL CORREGIDA ✅
 // src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; 
 import App from "./App";
 import "./styles/styles.css";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/CyberNova-Solutions">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CyberNova-Solutions/', // 👈 exacto igual que tu repo
+  //base: '/CyberNova-Solutions/', // 👈 exacto igual que tu repo
 })
